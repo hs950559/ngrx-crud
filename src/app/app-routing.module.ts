398f8cdae5eac0d9f07a12c3af58ddb0f3ba9990
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-04-11 21:11:35 +0530

    initial commit - with rest server

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  {
+    path: "lazy",
+    loadChildren: () => import("./lazy/lazy.module").then((m) => m.LazyModule),
+  },
+  {
+    path: "counter",
+    loadChildren: () =>
+      import("./counter/counter.module").then((m) => m.CounterModule),
+  },
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full",
+  },
+  { path: "**", component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
