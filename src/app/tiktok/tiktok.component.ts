a01eb29ae406709a3b035c1e50644a38b8f35ddd
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-04-13 00:07:44 +0530

    skills form array

--- src/app/tiktok/tiktok.component.ts
+++ src/app/tiktok/tiktok.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TiktokService } from './services/tiktok.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
 import { tap } from 'rxjs/operators';
 
 @Component({
@@ -11,23 +17,48 @@ import { tap } from 'rxjs/operators';
 export class TiktokComponent implements OnInit {
   form: FormGroup;
   users$: any;
+  techSkills: any[] = [
+    { name: 'Java', value: 'java' },
+    { name: 'PHP', value: 'php' },
+    { name: '.NET', value: '.net' },
+    { name: 'Node JS', value: 'nodejs' },
+  ];
   constructor(private tiktokService: TiktokService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      email: ['', { updateOn: 'blur' }],
+      email: ['', { validators: [Validators.required], updateOn: 'blur' }],
       password: ['', { updateOn: 'blur' }],
       city: ['', { updateOn: 'blur' }],
       state: [''],
       zip: ['', { updateOn: 'blur' }],
       address: ['', { updateOn: 'blur' }],
       color: [''],
-      skill: [''],
+      skills: this.fb.array([]),
       rememberMe: [''],
     });
     this.users$ = this.tiktokService.getUsers();
 
     this.form.valueChanges.pipe(tap((val) => console.log(val))).subscribe();
+  }
+
+  get skills() {
+    return this.form.get('skills') as FormArray;
+  }
+
+  onCheckboxChange(e) {
+    if (e.target.checked) {
+      this.skills.push(new FormControl(e.target.value));
+    } else {
+      let i = 0;
+      this.skills.controls.forEach((item: FormControl) => {
+        if (item.value === e.target.value) {
+          this.skills.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
   }
 
   saveForm() {
